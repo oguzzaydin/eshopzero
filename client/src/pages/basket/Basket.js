dfff82908be0b066b9c01061c5d5862bff5ffb92
Oğuzhan Aydın <oguzhan.aydin@medyasoft.com.tr>
2021-04-28 18:00:52 +0300

    basket page update :tada:

--- client/src/pages/basket/Basket.js
+++ client/src/pages/basket/Basket.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { List, Typography, Button, message, Result } from "antd";
+import { List, Typography, Button, message, Result, Spin } from "antd";
 import BasketItem from "./BasketItem";
 import { BasketContext } from "./BasketContext";
 import OrderService from "../order/OrderService";
@@ -9,9 +9,11 @@ const { Title } = Typography;
 
 const orderService = new OrderService();
 
-const Basket = ({history}) => {
+const Basket = ({ history }) => {
   const connection = createSocketConnection();
   const [ready, setReady] = useState(true);
+  const [showResult, setShowResult] = useState(false);
+
   const [error, setError] = useState(null);
   const { basket, count, updateBasket, resetCount } = useContext(BasketContext);
 
@@ -20,6 +22,7 @@ const Basket = ({history}) => {
     connection.on("ProductStockChanged", (result) => {
       console.log(result);
       setReady(true);
+      setShowResult(true);
       message.success(`Sipariş başarıyla oluşturuldu.`);
       updateBasket([]);
       resetCount();
@@ -41,33 +44,56 @@ const Basket = ({history}) => {
       .catch((error) => message.error(error.title));
   };
 
-  return !ready ? (
+  return showResult ? (
     <Result
-      status="success"
-      title="Successfully Purchased Cloud Server ECS!"
-      subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+      status={error != null ? "error" : "success"}
+      s
+      title={
+        error != null
+          ? "Error was occurred"
+          : "Successfully Purchased Cloud Server ECS!"
+      }
+      subTitle={
+        error != null
+          ? error
+          : "Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+      }
       extra={[
         <Button type="primary" key="console" onClick={() => history.push("/")}>
           Go Products
-        </Button>
+        </Button>,
       ]}
     />
   ) : (
     <>
-      <Title level={2}>Basket {count > 0 && `(${count})`} </Title>
+      {!ready ? (
+        <Spin
+          size="large"
+          style={{
+            position: "absolute",
+            bottom: "50%",
+            left: "50%",
+            zIndex: "9999999",
+          }}
+        />
+      ) : (
+        <div>
+          <Title level={2}>Basket {count > 0 && `(${count})`} </Title>
 
-      <List
-        itemLayout="horizontal"
-        dataSource={basket}
-        renderItem={(item) => <BasketItem item={item} />}
-      />
-      <br />
-      <br />
+          <List
+            itemLayout="horizontal"
+            dataSource={basket}
+            renderItem={(item) => <BasketItem item={item} />}
+          />
+          <br />
+          <br />
 
-      {count > 0 && (
-        <Button type="primary" onClick={createOrder}>
-          Sepeti Onayla
-        </Button>
+          {count > 0 && (
+            <Button type="primary" onClick={createOrder}>
+              Sepeti Onayla
+            </Button>
+          )}
+        </div>
       )}
     </>
   );
---
+++ services/order/Order.Api/Application/Validations/CreateOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using Order.Api.Application.Commands;
+
+namespace Order.Api.Application.Validations
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator(ILogger<CreateOrderCommand> logger)
+        {
+            RuleFor(order => order.Items).NotNull().NotEmpty();
+
+            logger.LogTrace("----- INSTANCE CREATED - {ClassName}", GetType().Name);
+        }
+    }
+
+    public class OrderITemModelValidator : AbstractValidator<OrderItemModel>
+    {
+        public OrderITemModelValidator(ILogger<CreateOrderCommand> logger)
+        {
+            RuleFor(item => item.ProductId).NotNull().NotEmpty();
+            RuleFor(item => item.Quantity).Must((item, _) => item.Quantity > 0).NotNull().NotEmpty();
+
+            logger.LogTrace("----- INSTANCE CREATED - {ClassName}", GetType().Name);
+        }
+    }
+}
